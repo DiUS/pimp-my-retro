2bc75ca1bbe2aff39b2aff52ae6da08a8ae1b254
timothy-clifford <tim.clifford@pawsforlife.com.au>
2014-03-28 14:00:29 +1100

    addint validation, errors, and colors to notes

--- src/js/angular/controllers/notes.js
+++ src/js/angular/controllers/notes.js
@@ -2,18 +2,29 @@ app.controller('NotesCtrl', ['$scope', '$rootScope', 'TopicService', 'CategorySe
 	$scope.topic = {};
   $scope.topics = TopicService.list();
   $scope.categories = CategoryService.list();
-  $scope.selectedCategory = {};
+  $scope.selectedCategory = false;
 
   $scope.selectCategory = function(category) {
     $scope.selectedCategory = category;
-  }
+  };
 
   $scope.addTopic = function() {
     $scope.topic['category'] = $scope.selectedCategory;
-    TopicService.add($scope.topic);
-  }
+    if ($scope.topic.text) {
+    	TopicService.add($scope.topic);
+    	$scope.error = false;
+    	$scope.selectedCategory = false;
+    } else {
+    	$scope.error = "Add a topic, stupid!";
+    }
+    $scope.topic = {};
+  };
   
   $scope.vote = function(topic) {
     TopicService.vote(topic, $rootScope.user.given_name);
   }
+  
+  $scope.showInput = function() {
+	  return selectedCategory;
+  };
 }]);
