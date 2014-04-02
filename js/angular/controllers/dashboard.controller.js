bb0561dbcf49fc4fdcfa1bdabd71cc4494e543ed
timothy-clifford <tim.clifford@pawsforlife.com.au>
2014-04-02 22:12:14 +1100

    adding drag and drop. TODO: sort out the logic that groups the topics

--- js/angular/controllers/dashboard.controller.js
+++ js/angular/controllers/dashboard.controller.js
@@ -1,4 +1,4 @@
-var controller = app.controller('DashboardCtrl', ['$scope', '$location', 'CategoryService', 'TopicService', 'ActionService', function($scope, $location, CategoryService, TopicService, ActionService) {
+var controller = app.controller('DashboardCtrl', ['$scope', '$location', '$log', 'CategoryService', 'TopicService', 'ActionService', function($scope, $location, $log, CategoryService, TopicService, ActionService) {
     $scope.title = 'Dashboard';
     $scope.categories = CategoryService.list();
     $scope.topics = TopicService.list();
@@ -8,16 +8,15 @@ var controller = app.controller('DashboardCtrl', ['$scope', '$location', 'Catego
     $scope.list2 = {};
 
     $scope.dropped = function($event, $index, topic) {
-        alert("remove " + topic.topicGroup.title);
+        alert("remove " + topic.text);
+        //TODO remove the topic from the UI
     };
 
+    // $data is the topic that is being dropped,
+    // targetTopic is the topic that is being dropped onto.
     $scope.drop = function($event, $data, targetTopic) {
-        if (targetTopic.topicGroup == undefined) {
-        	targetTopic.topicGroup = {title: targetTopic.text, topics: new Array()};
-        }
-        targetTopic.topicGroup.topics.push(targetTopic);
-        targetTopic.topicGroup.topics.push($data);
-        $data.topicGroup = targetTopic.topicGroup;
+    	$log.debug('drop: ' + $data.text);
+    	//TODO add the $data topic to the targetTopic
     };
 
     $scope.remove = function(topic) {
