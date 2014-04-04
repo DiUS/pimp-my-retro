38a561e36895a3ecb5df2382aca5e2cdf1490377
Bryan Signey <bsigney@dius.com.au>
2014-04-04 15:25:02 +1100

    Success message when adding actions. changes to action display in dashboard

--- js/angular/controllers/actions.controller.js
+++ js/angular/controllers/actions.controller.js
@@ -1,12 +1,14 @@
 app.controller('ActionsCtrl', ['$scope', 'ActionService', function($scope, ActionService) {
   $scope.action = {};
   $scope.actions = ActionService.list();
+  $scope.message = '';
   
   $scope.selectedTopic = ActionService.getSelectedTopic();
   
-  $scope.addAction = function(action) {
+  $scope.addAction = function() {
     $scope.action['topic'] = $scope.selectedTopic;
     ActionService.add($scope.action);
+    $scope.message = 'Action added';
 
     $scope.selectedTopic = {};
   }
